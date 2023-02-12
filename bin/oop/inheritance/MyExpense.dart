9628f1c09e2af20e587dc0de17e9815693d3de8a
Mahabub Alam Shawon <mahabub15-8580@diu.edu.bd>
2023-02-12 22:59:21 +0600

    add method override

--- bin/oop/inheritance/MyExpense.dart
+++ bin/oop/inheritance/MyExpense.dart
@@ -1,3 +1,13 @@
 import 'MyAccount.dart';
 
-class MyExpense extends MyAccount {}
+class MyExpense extends MyAccount {
+  //we can modified any method of MyAccount Class using override
+
+  var eidBonus = 10000;
+
+  @override
+  void addDeposit(int add) {
+    var updateBalance = myBalance + add + eidBonus;
+    print('My Update Balance is $updateBalance after deposite with bonus');
+  }
+}
